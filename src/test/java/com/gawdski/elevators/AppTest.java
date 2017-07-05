01afa83fa44157907a7e1d6a9f7224b41b9fb569
Sergey <Siarhei_Schauchenka@epam.com>
2017-07-05 13:35:11 +0200

    Added test with data provider

--- src/test/java/com/gawdski/elevators/AppTest.java
+++ src/test/java/com/gawdski/elevators/AppTest.java
@@ -1,5 +1,34 @@
 package com.gawdski.elevators;
 
+import elevator.runner.ConsoleRunner;
+import elevator.runner.RunnerConfiguration;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test
 public class AppTest {
+
+    @DataProvider(name = "testData")
+    public Object[][] provide(){
+        return new Object[][]{
+                {new RunnerConfiguration(
+                        10,
+                        5,
+                        new Main.MyRoutingController(),
+                        new Main.MyButtonPressGenerator()
+                )},
+
+        };
+    }
+
+    @Test(dataProvider = "testData")
+    public void elevatorTest(RunnerConfiguration config){
+        try{
+            ConsoleRunner.runWith(config);
+        } catch (InterruptedException e){
+            e.getLocalizedMessage();
+        }
+    }
+
 
 }
